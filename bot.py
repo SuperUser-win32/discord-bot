2011501f2a58068a916fce45e12a27e49836ad79
SuperUser-win32 <gttyeio@gmail.com>
2026-01-26 15:43:20 +0100

    adding basic on_join and leave msgs

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord import Embed, Member, Intents, Client, Color
+from contextlib import suppress
 import os
-import discord
 import logging
 
-Bot_logger = logging.Logger("DISCORD-BOT")
+Bot_logger = logging.Logger("discord.bot")
 Formatter = logging.Formatter("%(asctime)s | %(name)s | %(message)s")
 file_handler = logging.FileHandler("logs.log")
 stream_handler = logging.StreamHandler()
@@ -14,7 +16,16 @@ Bot_logger.addHandler(stream_handler)
 
 load_dotenv()
 
-client = discord.Client(discord.Intents.all())
+client = Client(intents=Intents.all())
+bot_user_id = int(os.getenv("BOT_USER_ID"))
+welcoming_channel_id = int(os.getenv("WELCOMING_CHANNEL_ID"))
+
+bot = commands.Bot("!", intents=Intents.all())
+
+
+async def send_to_channel(channel_id: int, *args, **kwargs) -> None:
+    channel = client.get_channel(channel_id)
+    await channel.send(*args, **kwargs)
 
 
 @client.event
@@ -22,4 +33,28 @@ async def on_ready():
     Bot_logger.info("The bot has started")
 
 
-client.run(os.getenv("DISCORD_API_KEY"))
+@client.event
+async def on_member_remove(member: Member):
+    embed_sent = Embed(
+        title=f"Bye {member.name}",
+        description="We wish you the best",
+        color=Color.dark_red(),
+    )
+    with suppress(AttributeError):
+        embed_sent.set_thumbnail(url=member.avatar.url)
+    await send_to_channel(welcoming_channel_id, embed=embed_sent)
+
+
+@client.event
+async def on_member_join(member: Member):
+    embed_sent = Embed(
+        title=f"Welcome {member.name}",
+        description="We welcome you into the server",
+        color=Color.dark_blue(),
+    )
+    with suppress(AttributeError):
+        embed_sent.set_thumbnail(url=member.avatar.url)
+    await send_to_channel(welcoming_channel_id, embed=embed_sent)
+
+
+client.run(os.getenv("DISCORD_API_KEY"), log_formatter=Formatter)
